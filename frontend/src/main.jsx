0ef462a9fda2ef4e946223433ab6ae1c5a6979c6
Yohoji <40741767+yohoji@users.noreply.github.com>
2025-09-19 20:16:35 +0000

    fix: provider error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { ChakraProvider} from '@chakra-ui/react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "./components/ui/provider.jsx";
 
-createRoot(document.getElementById('root')).render(
- 
- <StrictMode>
-    
-    <ChakraProvider>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider>
       <App />
-    </ChakraProvider>
-    
+    </Provider>
   </StrictMode>
-)
+);
